39b6003c829f42eb4113a31c07adffbe744cad62
Arne Emil Buchter <5651612@RTS-365.dk>
2020-02-11 12:54:33 +0100

    sfrv

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+    
 
     let colorclick = document.querySelector('.clickme');
 
@@ -13,30 +13,14 @@
     })
     
     showBox.innerHTML += arrOne;
+
+    fetch("https://api.hejmeddig.com/v1/sider", {
+        method: "GET"
+      }).then(function (response) {
+        return response.json();
+      }).then(function (data) {
+         console.log(data); 
+        })
     
     //foo();
-
-    try {
-        let a = undefinedVariable
-      } catch (e) {
-        console.log(e instanceof ReferenceError)  // true
-        console.log(e.message)                    // "undefinedVariable is not defined"
-        console.log(e.name)                       // "ReferenceError"
-        console.log(e.fileName)                   // "Scratchpad/1"
-        console.log(e.lineNumber)                 // 2
-        console.log(e.columnNumber)               // 6
-        console.log(e.stack)                      // "@Scratchpad/2:2:7\n"
-      }
-
-    try {
-        throw new ReferenceError('Hello', 'someFile.js', 10)
-      } catch (e) {
-        console.log(e instanceof ReferenceError)  // true
-        console.log(e.message)                    // "Hello"
-        console.log(e.name)                       // "ReferenceError"
-        console.log(e.fileName)                   // "someFile.js"
-        console.log(e.lineNumber)                 // 10
-        console.log(e.columnNumber)               // 0
-        console.log(e.stack)                      // "@Scratchpad/2:2:9\n"
-      }
 

321e9b33b3b0bde2c64ecfbd97bb9c78be3ee261
Arne Emil Buchter <5651612@RTS-365.dk>
2020-02-11 12:48:05 +0100

    dd

--- index.js
+++ index.js
@@ -17,6 +17,18 @@
     //foo();
 
     try {
+        let a = undefinedVariable
+      } catch (e) {
+        console.log(e instanceof ReferenceError)  // true
+        console.log(e.message)                    // "undefinedVariable is not defined"
+        console.log(e.name)                       // "ReferenceError"
+        console.log(e.fileName)                   // "Scratchpad/1"
+        console.log(e.lineNumber)                 // 2
+        console.log(e.columnNumber)               // 6
+        console.log(e.stack)                      // "@Scratchpad/2:2:7\n"
+      }
+
+    try {
         throw new ReferenceError('Hello', 'someFile.js', 10)
       } catch (e) {
         console.log(e instanceof ReferenceError)  // true

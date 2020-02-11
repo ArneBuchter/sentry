9564b7aa8754e782f2e68c6c3cf1742ed2cda924
Arne Emil Buchter <5651612@RTS-365.dk>
2020-02-11 12:36:26 +0100

    ghej

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-//document.addEventListener('DOMContentLoaded', function () {
 
-    let colorclick = document.querySelector('clickme');
 
-    console.log(colorclick);
-    
+    let colorclick = document.querySelector('.clickme');
+
     let color = document.querySelector('.colorone');
     let arrOne = ['hej', 'farvel', 'ses', 'snakkes'];
     let showBox = document.querySelector('.show');
@@ -16,5 +14,17 @@
     
     showBox.innerHTML += arrone;
     
-    foo();
-//})
+    //foo();
+
+    try {
+        let a = undefinedVariable
+      } catch (e) {
+        console.log(e instanceof ReferenceError)  // true
+        console.log(e.message)                    // "undefinedVariable is not defined"
+        console.log(e.name)                       // "ReferenceError"
+        console.log(e.fileName)                   // "Scratchpad/1"
+        console.log(e.lineNumber)                 // 2
+        console.log(e.columnNumber)               // 6
+        console.log(e.stack)                      // "@Scratchpad/2:2:7\n"
+      }
+

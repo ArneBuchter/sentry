5ef57747a6a51a6e3a2eb174ba34ab5077a98eec
Arne Emil Buchter <5651612@RTS-365.dk>
2020-02-11 13:32:13 +0100

    math

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-    
-
     let colorclick = document.querySelector('.clickme');
     const number = 24;
     let color = document.querySelector('.colorone');
     let arrOne = ['hej', 'farvel', 'ses', 'snakkes'];
     let showBox = document.querySelector('.show');
+    let one = 1;
+    let two = "2";
+
+    let result = one + two;
+
+    console.log(result);
     
     colorclick.addEventListener('click', () => {
         
@@ -14,7 +18,10 @@
     
     showBox.innerHTML += arrOne;
 
-    number = 12;
     
+
+    // number = 12;
+    
+
     //foo();
 

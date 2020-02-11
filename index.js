2247684a40f69ac56e309c958668d97d89d0bbfa
Arne Emil Buchter <5651612@RTS-365.dk>
2020-02-11 12:39:13 +0100

    kkl

--- index.js
+++ index.js
@@ -17,14 +17,14 @@
     //foo();
 
     try {
-        let a = undefinedVariable
+        throw new ReferenceError('Hello', 'someFile.js', 10)
       } catch (e) {
         console.log(e instanceof ReferenceError)  // true
-        console.log(e.message)                    // "undefinedVariable is not defined"
+        console.log(e.message)                    // "Hello"
         console.log(e.name)                       // "ReferenceError"
-        console.log(e.fileName)                   // "Scratchpad/1"
-        console.log(e.lineNumber)                 // 2
-        console.log(e.columnNumber)               // 6
-        console.log(e.stack)                      // "@Scratchpad/2:2:7\n"
+        console.log(e.fileName)                   // "someFile.js"
+        console.log(e.lineNumber)                 // 10
+        console.log(e.columnNumber)               // 0
+        console.log(e.stack)                      // "@Scratchpad/2:2:9\n"
       }
 
